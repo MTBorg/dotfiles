2a729d33131b600f08aa37c81354047154d7855e
MTBorg <martinterneborg96@gmail.com>
2019-07-15 22:12:04 +0200

    Rewrite vim init script to use symbolic links instead of copying

--- viminit.sh
+++ viminit.sh
@@ -1,4 +1,7 @@
-eval "cp .vimrc ~ &&
-	cd .vim &&
-	git submodule update --init --recursive &&
-	cp -r . ~/.vim"
+git submodule update --init --recursive
+ln -s -f ~/.config/.vimrc ~/.vimrc
+ln -s -f ~/.config/.vim/ ~/.vim
+#eval "cp .vimrc ~ &&
+#	cd .vim &&
+#	git submodule update --init --recursive &&
+#	cp -r . ~/.vim"
