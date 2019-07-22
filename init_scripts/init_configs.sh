a1ce3043167ef339ef69a124d8a9bd818d5f7cf3
MTBorg <martinterneborg96@gmail.com>
2019-07-22 10:58:13 +0200

    Add backup to config init

--- init_scripts/init_configs.sh
+++ init_scripts/init_configs.sh
@@ -1,6 +1,3 @@
-cd ..
-directories=$(ls -d */ | sed 's/\///')
-
 # Directories to ignore, any directory names placed in this array won't be symlinked.
 # Useful if some configs don't go into directory $config_directory.
 ignored_directories=( 
@@ -14,6 +11,44 @@ ignored_directories=(
 
 # The directory in which to place the symlinks.
 config_directory=~/config_test/
+backup_directory=~/config_backup/
+
+# Backup the current config
+if [ -d $config_directory ]; then
+	if [ -d $backup_directory ]; then
+		echo "Backup directory $backup_directory already exists. Overwrite?(y/n)"
+		read input
+		if [ $input == "y" ]; then
+			echo "Overwriting backup"
+			echo "Creating backup of existing config directory $config_directory"
+			test=$(sudo cp -rf $config_directory $backup_directory)
+		elif [ $input == "n" ]; then
+			echo "Skipping overwriting backup, exiting..."
+			exit 1
+		else
+			echo "Received invalid input $input, exiting..."
+			exit 1
+		fi
+	else
+		echo "Creating backup of existing config directory $config_directory"
+		test=$(sudo cp -rf $config_directory $backup_directory)
+	fi
+else
+	echo "Could not find folder $config_directory. Skip backup and continue?(y/n)"
+	read input
+	if [ $input == "y" ]; then
+		echo "Skipping backup..."
+	elif [ $input == "n" ]; then
+		echo "Won't skip backup, exiting..."
+		exit 1
+	else
+		echo "Received invalid input $input, exiting..."
+		exit 1
+	fi
+fi
+
+cd ..
+directories=$(ls -d */ | sed 's/\///')
 
 # Make sure the config directory actually exists
 echo "Creating config directory $config_directory"
