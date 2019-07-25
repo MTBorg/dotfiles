122e5490937194d1f2eb51ac63245c336dd4e96e
MTBorg <martinterneborg96@gmail.com>
2019-07-25 12:35:12 +0200

    Remove polybar init script

--- init_scripts/polybar_init.sh
+++
@@ -1,4 +0,0 @@
-# Copies the config files to .config and the spotify status script to the home folder.
-eval "mkdir -p ~/.config/polybar/ &&
-			cp -t ~/.config/polybar/ polybar/config polybar/launch.sh &&
-			cp polybar/polybar-spotify/spotify_status.py ~"
