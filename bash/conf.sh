182214fa02bbe9b7f92dbf3c53e72078d0ee1fbb
MTBorg <martinterneborg96@gmail.com>
2019-08-13 21:01:55 +0200

    Add color to man pages

--- bash/conf.sh
+++ bash/conf.sh
@@ -17,4 +17,13 @@ export PS1="\e[0;32m[\u@\h \W]$ \e[m"
 # Use vim as standard editor
 export EDITOR="vim"
 
+# Color man pages
+export LESS_TERMCAP_mb=$'\e[1;32m'
+export LESS_TERMCAP_md=$'\e[1;32m'
+export LESS_TERMCAP_me=$'\e[0m'
+export LESS_TERMCAP_se=$'\e[0m'
+export LESS_TERMCAP_so=$'\e[01;33m'
+export LESS_TERMCAP_ue=$'\e[0m'
+export LESS_TERMCAP_us=$'\e[1;4;31m'
+
 source ~/.config/z/z.sh
