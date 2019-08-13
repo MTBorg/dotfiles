840af8004a74e85d746c7aa7f6b6692ed971ee9c
MTBorg <martinterneborg96@gmail.com>
2019-08-13 21:20:55 +0200

    Update bash man pages colors

--- bash/conf.sh
+++ bash/conf.sh
@@ -18,12 +18,11 @@ export PS1="\e[0;32m[\u@\h \W]$ \e[m"
 export EDITOR="vim"
 
 # Color man pages
-export LESS_TERMCAP_mb=$'\e[1;32m'
-export LESS_TERMCAP_md=$'\e[1;32m'
-export LESS_TERMCAP_me=$'\e[0m'
-export LESS_TERMCAP_se=$'\e[0m'
-export LESS_TERMCAP_so=$'\e[01;33m'
-export LESS_TERMCAP_ue=$'\e[0m'
-export LESS_TERMCAP_us=$'\e[1;4;31m'
+export LESS_TERMCAP_md=$'\e[01;32m' # Bold start
+export LESS_TERMCAP_me=$'\e[0m' # Bold end
+export LESS_TERMCAP_so=$'\e[01;44;33m' # Standout-mode start
+export LESS_TERMCAP_se=$'\e[0m' # Standout-mode end
+export LESS_TERMCAP_us=$'\e[01;32m' # Underline start
+export LESS_TERMCAP_ue=$'\e[0m' # Underline end
 
 source ~/.config/z/z.sh
