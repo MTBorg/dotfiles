6cad6e1a164d807544dd01df35cff43d780917e8
Martin Terneborg <martinterneborg@protonmail.com>
2020-04-02 19:43:48 +0200

    (Aliases)Don't check if programs are installed before aliasing

--- aliases/.config/aliases/.aliases.sh
+++ aliases/.config/aliases/.aliases.sh
@@ -4,9 +4,7 @@ alias h="history"
 alias ll="ls -al --color=auto"
 alias ls="ls --color=auto"
 alias grep="grep --color=auto"
-if ! [ -x git ]; then 
-	alias g="git"
-fi
+alias g="git"
 alias grep="grep --color=auto"
 if ! [ -x bat ]; then # Use bat if installed
 	alias cat="bat"
@@ -17,12 +15,8 @@ alias sysrestart="systemctl restart"
 alias sysstatus="systemctl status"
 alias sysenable="systemctl enable"
 alias sysdisable="systemctl disable"
-if ! [ -x docker ]; then
-	alias d="docker"
-fi
-if ! [ -x docker-compose ]; then
-	alias dc="docker-compose"
-fi
+alias d="docker"
+alias dc="docker-compose"
 
 # Arch Linux specific aliases
 if [ -f "/etc/arch-release" ]; then
