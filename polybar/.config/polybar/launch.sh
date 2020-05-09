4d2abcedbe928defe9769ccee5aebdd19c3c17b8
Martin Terneborg <martinterneborg@protonmail.com>
2020-05-09 22:53:57 +0200

    Add dual-monitor support

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,5 +6,8 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar  > /dev/null; do sleep 1; done
 
-# Launch Polybar, using default config location
-polybar mybar &
+# Launch Polybar, using default config location, on all connected monitors
+for monitor in $(xrandr -q | grep " connected " | awk '{print $1}')
+do
+	MONITOR=$monitor polybar mybar &
+done
