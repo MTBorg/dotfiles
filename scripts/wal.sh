c198d00bf01d4882bcc865f0c6690dd2b9c8ec7c
Martin Terneborg <martinterneborg@protonmail.com>
2020-05-09 23:44:51 +0200

    (Scripts)Add script for selecting pywal image using fzf

--- scripts/wal.sh
+++ scripts/wal.sh
@@ -2,3 +2,8 @@
 waln() {
 	wal -i ~/Pictures/wallpapers --iterative; spicetify update
 }
+
+# Select a picture to use with wal using fzf
+fzwal(){
+	ls ~/Pictures/wallpapers | fzf --bind "enter:execute(wal -i ~/Pictures/wallpapers/{})"
+}
