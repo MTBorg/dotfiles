e44241403b49540b7c13c52fbd3ca720e61e8166
Martin Terneborg <martinterneborg@protonmail.com>
2021-01-29 14:02:01 +0100

    (Polybar)Fix bluetooth script

--- polybar/.config/polybar/polybar-scripts/system-bluetooth-bluetoothctl.sh
+++ polybar/.config/polybar/polybar-scripts/system-bluetooth-bluetoothctl.sh
@@ -3,34 +3,30 @@
 bluetooth_print() {
     bluetoothctl | while read -r; do
         if [ "$(systemctl is-active "bluetooth.service")" = "active" ]; then
-
             devices_paired=$(bluetoothctl paired-devices | grep Device | cut -d ' ' -f 2)
             counter=0
 
-            echo "$devices_paired" | while read -r line; do
-                device_info=$(bluetoothctl info "$line")
-
+            for device in $devices_paired; do
+                device_info=$(bluetoothctl info "$device")
                 if echo "$device_info" | grep -q "Connected: yes"; then
-                		device_alias=$(echo "$device_info" | grep "Alias" | cut -d ' ' -f 2-)
-										printf ''
-
+                    device_alias=$(echo "$device_info" | grep "Alias" | cut -d ' ' -f 2-)
+                    printf ''
                     if [ $counter -gt 0 ]; then
                         printf ", %s" "$device_alias"
                     else
                         printf " %s" "$device_alias"
                     fi
-
                     counter=$((counter + 1))
                 fi
-
-                printf '\n'
             done
 
             if [ $counter -eq 0 ]; then
                 echo 
+            else
+                printf '\n'
             fi
         else
-            echo "#2"
+            echo 
         fi
     done
 }
