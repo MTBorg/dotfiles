fe77a8f3633c9bb44ecca87796074910ed462ca2
Martin Terneborg <martinterneborg@protonmail.com>
2021-06-23 22:43:27 +0200

    (Polybar)Fix issue with polybar not starting with multiple monitors

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -10,4 +10,7 @@ while pgrep -u $UID -x polybar  > /dev/null; do sleep 1; done
 for monitor in $(xrandr -q | grep " connected " | awk '{print $1}')
 do
 	MONITOR=$monitor polybar --config=$XDG_CONFIG_HOME/polybar/config.ini -r $POLYBAR_BAR &
+	# TODO: For some reason it seems that polybar fails to start if it launched
+	# to soon after another has been started.
+	sleep 0.5
 done
