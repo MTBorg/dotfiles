e9d23c0d38c31f82ddc3744eb3ff20ba87bcf4bf
Martin Terneborg <martinterneborg@protonmail.com>
2022-10-31 21:44:54 +0100

    Nvim: Try out tree-sitter incremental selection

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -202,6 +202,15 @@ vim.api.nvim_create_autocmd("bufwritepost", {
 			-- Instead of true it can also be a list of languages
 			additional_vim_regex_highlighting = false,
 		},
+		incremental_selection = {
+			enable = true,
+			keymaps = {
+				init_selection = "gnn",
+				node_incremental = "grn",
+				scope_incremental = "grc",
+				node_decremental = "grm",
+			},
+		},
 	}
 end)();
 
