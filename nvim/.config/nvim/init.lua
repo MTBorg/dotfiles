017362193796d87875d5db051c8fa1b1695d4f78
Martin Terneborg <martinterneborg@protonmail.com>
2024-12-08 22:59:18 +0100

    nvim: move treesitter config to separate file

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -220,71 +220,6 @@ vim.api.nvim_create_autocmd("bufwritepost", {
 	pattern = "$MYVIMRC"
 });
 
--- Treesitter
-(function()
-	require 'nvim-treesitter.configs'.setup {
-		ensure_installed = {
-			"bash",
-			"c",
-			"cmake",
-			"cpp",
-			"css",
-			"diff",
-			"dockerfile",
-			"go",
-			"gomod",
-			"gitignore",
-			"graphql",
-			"hcl",
-			"html",
-			"http",
-			"javascript",
-			"json",
-			"json5",
-			"make",
-			"markdown",
-			"proto",
-			"python",
-			"rasi",
-			"rust",
-			"sql",
-			"toml",
-			"typescript",
-			"vim",
-			"vimdoc",
-			"yaml",
-		},
-		auto_install = true,
-		highlight = {
-			enable = true,
-			-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-			-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-			-- Using this option may slow down your editor, and you may see some duplicate highlights.
-			-- Instead of true it can also be a list of languages
-			additional_vim_regex_highlighting = false,
-		},
-		incremental_selection = {
-			enable = true,
-			keymaps = {
-				init_selection = "<C-n>",
-				node_incremental = "<C-n>",
-				node_decremental = "<C-m>",
-				scope_incremental = "<C-b>",
-			},
-		},
-		textobjects = {
-			select = {
-				enable = true,
-				lookahead = true,
-				keymaps = {
-					["af"] = "@function.outer",
-					["if"] = "@function.inner",
-				},
-			}
-		}
-	}
-end)();
-
 -- nvim-tree
 (function()
 	require("nvim-tree").setup(
@@ -534,6 +469,7 @@ end)();
 require 'plugin/lsp'
 require 'plugin/cmp'
 require 'plugin/telescope'
+require 'plugin/treesitter'
 
 vim.keymap.set('n', '<leader>fa', vim.lsp.buf.code_action)
 
---
+++ nvim/.config/nvim/lua/plugin/treesitter.lua
@@ -0,0 +1,61 @@
+require 'nvim-treesitter.configs'.setup {
+		ensure_installed = {
+			"bash",
+			"c",
+			"cmake",
+			"cpp",
+			"css",
+			"diff",
+			"dockerfile",
+			"go",
+			"gomod",
+			"gitignore",
+			"graphql",
+			"hcl",
+			"html",
+			"http",
+			"javascript",
+			"json",
+			"json5",
+			"make",
+			"markdown",
+			"proto",
+			"python",
+			"rasi",
+			"rust",
+			"sql",
+			"toml",
+			"typescript",
+			"vim",
+			"vimdoc",
+			"yaml",
+		},
+		auto_install = true,
+		highlight = {
+			enable = true,
+			-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+			-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+			-- Using this option may slow down your editor, and you may see some duplicate highlights.
+			-- Instead of true it can also be a list of languages
+			additional_vim_regex_highlighting = false,
+		},
+		incremental_selection = {
+			enable = true,
+			keymaps = {
+				init_selection = "<C-n>",
+				node_incremental = "<C-n>",
+				node_decremental = "<C-m>",
+				scope_incremental = "<C-b>",
+			},
+		},
+		textobjects = {
+			select = {
+				enable = true,
+				lookahead = true,
+				keymaps = {
+					["af"] = "@function.outer",
+					["if"] = "@function.inner",
+				},
+			}
+		}
+	}
