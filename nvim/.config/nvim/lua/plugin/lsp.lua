99216f32ca0686d2a9efddb49da955ba821d6f91
Martin Terneborg <martinterneborg@protonmail.com>
2024-12-20 20:14:11 +0100

    nvim: configure language servers explicitly

--- nvim/.config/nvim/lua/plugin/lsp.lua
+++ nvim/.config/nvim/lua/plugin/lsp.lua
@@ -1,21 +1,11 @@
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local lspconfig = require('lspconfig')
 
-require 'lspconfig'.gopls.setup {}
-require 'lspconfig'.lua_ls.setup {}
 require 'lazydev'.setup {}
 
 vim.keymap.set('n', 'gf', vim.lsp.buf.rename)
 vim.keymap.set("n", "gn", vim.diagnostic.goto_next)
 vim.keymap.set("n", "gp", vim.diagnostic.goto_prev)
-
-local servers = { 'gopls' }
-for _, lsp in ipairs(servers) do
-	lspconfig[lsp].setup {
-		-- on_attach = my_custom_on_attach,
-		capabilities = capabilities,
-	}
-end
 
 vim.keymap.set('n', 'K', function()
 	require("pretty_hover").hover()
@@ -37,6 +27,14 @@ vim.api.nvim_create_autocmd('LspAttach', {
 	end,
 })
 
+lspconfig.gopls.setup {
+	capabilities = capabilities,
+}
+
+lspconfig.lua_ls.setup {
+	capabilities = capabilities,
+}
+
 lspconfig.volar.setup {
 	filetypes = { 'vue' },
 	init_options = {
@@ -49,4 +47,32 @@ lspconfig.volar.setup {
 lspconfig.elixirls.setup {
 	capabilities = capabilities,
 	cmd = { "/opt/homebrew/bin/elixir-ls" },
+}
+
+lspconfig.eslint.setup {
+	capabilities = capabilities,
+}
+
+lspconfig.ts_ls.setup {
+	capabilities = capabilities,
+}
+
+lspconfig.graphql.setup {
+	capabilities = capabilities,
+}
+
+lspconfig.dockerls.setup {
+	capabilities = capabilities,
+}
+
+lspconfig.docker_compose_language_service.setup {
+	capabilities = capabilities,
+}
+
+lspconfig.buf_ls.setup {
+	capabilities = capabilities,
+}
+
+lspconfig.pyright.setup {
+	capabilities = capabilities,
 }
