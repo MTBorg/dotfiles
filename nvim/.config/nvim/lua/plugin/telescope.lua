22b6d084c2508cfab186b93a921b5c5cfe6891fa
Martin Terneborg <martinterneborg@protonmail.com>
2025-09-17 21:17:57 +0200

    nvim: Add telescope keybinding for grepping word under cursor

--- nvim/.config/nvim/lua/plugin/telescope.lua
+++ nvim/.config/nvim/lua/plugin/telescope.lua
@@ -30,7 +30,8 @@ require("telescope").setup({
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fgg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fgc', builtin.grep_string, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>fc', builtin.commands, {})
