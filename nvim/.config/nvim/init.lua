c988da4cfb8a03b0131887b783fe19e1ae76bfa1
Martin Terneborg <martinterneborg@protonmail.com>
2026-06-15 17:32:46 +0200

    nvim: Replace plugin render-markdown with markdown-preview

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -170,6 +170,20 @@ require("lazy").setup({
 	'sindrets/diffview.nvim',
 	{ 'akinsho/git-conflict.nvim', version = "*", config = true },
 
+	{
+		"selimacerbas/markdown-preview.nvim",
+		dependencies = { "selimacerbas/live-server.nvim" },
+		config = function()
+			require("markdown_preview").setup({
+				-- all optional; sane defaults shown
+				instance_mode = "takeover", -- "takeover" (one tab) or "multi" (tab per instance)
+				port = 0,               -- 0 = auto (8421 for takeover, OS-assigned for multi)
+				open_browser = true,
+				debounce_ms = 300,
+			})
+		end,
+	},
+
 	-- telescope
 	{
 		'nvim-telescope/telescope.nvim',
